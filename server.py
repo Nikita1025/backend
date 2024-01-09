f8f7eb5d94c83a3370062aeec2be8b04e531dd14
Nikita1025 <nekit1025l@gmail.com>
2024-01-09 11:47:43 +0300

    create payment sub

--- server.py
+++ server.py
@@ -3,9 +3,7 @@ import json
 from flask_cors import CORS
 
 from flask import Flask, jsonify, request
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
 
 stripe.api_key = 'sk_test_51OVZadCH3dnwWZ7miWVnVfw0CddrO7Te4q6mrBIMw72cLJ8Ol3GigNU9KacNntWHt964iF7TCuegdbWDKoxLwf1500VXhvPL9p'
 
@@ -14,22 +12,46 @@ endpoint_secret = 'whsec_iFntZnznNPMbLvKS1fwXcMubEiUu4krR'
 app = Flask(__name__)
 CORS(app)
 
+user_info = {}
 
-@app.route('/pay', methods=['POST'])
-def pay():
+
+@app.route('/sub', methods=['POST'])
+def sub():
     email = request.json.get('email', None)
-    price = request.json.get('price', None)
+    payment_method = request.json.get('payment_method', None)
+    plan_id = request.json.get('plan_id', None)
 
     if not email:
         return 'You need to send an Email!', 400
+    if not payment_method:
+        return 'You need to send an payment_method!', 400
 
-    intent = stripe.PaymentIntent.create(
-        amount=price,
-        currency='usd',
-        receipt_email=email
+    # This creates a new Customer and attaches the default PaymentMethod in one API call.
+    customer = stripe.Customer.create(
+        payment_method=payment_method,
+        email=email,
+        invoice_settings={
+            'default_payment_method': payment_method,
+        },
+    )
+    # Creates a subscription and attaches the customer to it
+    subscription = stripe.Subscription.create(
+        customer=customer['id'],
+        items=[
+            {
+                'plan': plan_id,
+            },
+        ],
+        expand=['latest_invoice.payment_intent'],
     )
 
-    return {"client_secret": intent['client_secret']}, 200
+    status = subscription['latest_invoice']['payment_intent']['status']
+    client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
+
+    user_info['customer_id'] = customer['id']
+    user_info['email'] = email
+
+    return {'status': status, 'client_secret': client_secret}, 200
 
 
 @app.route('/create-payment-intent', methods=['POST'])
@@ -37,7 +59,8 @@ def create_payment():
     data = json.loads(request.data)
     payment_method_type = data['paymentMethodType']
     currency = data['currency']
-    formatted_payment_method_type = ['link', 'card'] if payment_method_type == 'link' else [payment_method_type]
+    formatted_payment_method_type = [
+        'link', 'card'] if payment_method_type == 'link' else [payment_method_type]
     params = {
         'payment_method_types': formatted_payment_method_type,
         'amount': 5999,
